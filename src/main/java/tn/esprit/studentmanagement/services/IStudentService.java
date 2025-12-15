1e070242335d190914d0335b7c43e5d73140c90b
Ahmed.dhib <115018064+Ahmeddhib@users.noreply.github.com>
2025-12-15 14:37:49 +0100

    Update IStudentService.java

--- src/main/java/tn/esprit/studentmanagement/services/IStudentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/IStudentService.java
@@ -1,12 +1,11 @@
 package tn.esprit.studentmanagement.services;
 
 import tn.esprit.studentmanagement.entities.Student;
-
 import java.util.List;
 
 public interface IStudentService {
-    public List<Student> getAllStudents();
-    public Student getStudentById(Long id);
-    public Student saveStudent(Student student);
-    public void deleteStudent(Long id);
+    List<Student> getAllStudents(); // Supprimer 'public' (implicite dans une interface)
+    Student getStudentById(Long id);
+    Student saveStudent(Student student);
+    void deleteStudent(Long id);
 }
