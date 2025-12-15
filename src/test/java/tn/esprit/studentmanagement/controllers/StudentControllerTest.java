bb281d4ab630227daa4883b0def9aa695ba3a4ed
ahmed dhib <ahmed@gmail.com>
2025-12-15 14:49:45 +0100

    modify some file

--- src/test/java/tn/esprit/studentmanagement/controllers/StudentControllerTest.java
+++ src/test/java/tn/esprit/studentmanagement/controllers/StudentControllerTest.java
@@ -2,9 +2,9 @@ package tn.esprit.studentmanagement.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import tn.esprit.studentmanagement.entities.Student;
@@ -27,7 +27,7 @@ class StudentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean  // Utiliser @MockBean pour Spring
+    @Mock  // Utiliser @MockBean pour Spring
     private IStudentService studentService;  // Mock de l'interface
 
     @Autowired
